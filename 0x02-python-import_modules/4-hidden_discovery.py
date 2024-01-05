2d4e0849e26d943489c95242f469435650f7cfe5
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-05 15:24:15 -0800

    commit 1

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+import dis
+
+def print_hidden_4_names():
+    code = open('hidden_4.pyc', 'rb').read()
+    instructions = dis.get_instructions(code)
+    
+    names = set()
+    for instr in instructions:
+        if instr.opname == 'LOAD_NAME' and not instr.argrepr.startswith('__'):
+            names.add(instr.argrepr)
+    
+    for name in sorted(names):
+        print(name)
+
+if __name__ == "__main__":
+    print_hidden_4_names()
