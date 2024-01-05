4ac09304a10f48c16e0f119e3b9708e3f4592f32
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-05 15:21:07 -0800

    commit 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,14 @@
 import sys
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
-
-    if num_args == 0:
+    argc = len(sys.argv) - 1
+    if argc == 0:
         print("0 arguments.")
-    elif num_args == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(num_args))
-
-    if num_args > 0:
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+        if argc == 1:
+            print("1 argument:")
+        else:
+            print(f"{argc} arguments:")
+        
+        for i, arg in enumerate(sys.argv[1:], 1):
+            print(f"{i}: {arg}")
