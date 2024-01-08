40d2838c42afd81d19d86513c5e4471d059c7f83
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-08 10:14:26 -0800

    commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if not my_list or idx < 0 or idx > len(my_list) or my_list is None or idx is None:
+    if not my_list or idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
