27f478e725f8d3b682e80e51b72e16a86e7dd3f9
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-08 10:48:53 -0800

    coomit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_str = ''
     for i in my_string:
-        if i == 'c' or i == 'C':
-            i = ''
-    return my_string
+        if i != 'c' or i != 'C':
+            new_str += i
+    return new_str
