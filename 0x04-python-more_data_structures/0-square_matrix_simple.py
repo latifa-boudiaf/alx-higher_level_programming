6920e215570d401304ec4fa3febc823c18cf0a96
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-12 07:08:08 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         row = []
-        for j in range(len(matrix[j])):
+        for j in range(len(matrix[i])):
             row.append(matrix[i][j] ** 2)
         
         new_matrix.append(row)
