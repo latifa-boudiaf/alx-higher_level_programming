49536ab0978ceab7bb5b8b4b83fa5823404b0aab
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-12 06:34:31 -0800

    commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ lastDigit = abs(number) % 10
 if number < 0:
     lastDigit = -lastDigit
 
-print("Last digit of", number, "is", last_digit, end=" ")
+print("Last digit of", number, "is", lastDigit, end=" ")
 
 if lastDigit > 5:
     print("and is greater than 5")
