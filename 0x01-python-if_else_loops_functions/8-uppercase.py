0ab019d8209d8a1c41444aa6e87e72e34b09023d
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-13 04:36:42 -0800

    c

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        upper_char = char
+        if ord('a') <= ord(char) <= ord('z'):
+            upper_char = chr(ord(char) - 32)
+        print("{:c}".format(upper_char), end="")
+    print()
