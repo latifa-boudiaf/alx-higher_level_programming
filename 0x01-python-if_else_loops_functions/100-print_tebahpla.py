2923b14134b6811f48260e27fddf5dcd5610765a
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-13 10:34:57 -0800

    c

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(i - 32 if i % 2 == 0 else i), end="")
+    print("{:c}".format(i - 32 if i % 2 != 0 else i), end="")
 
 print()
