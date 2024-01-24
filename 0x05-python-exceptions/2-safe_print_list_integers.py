64d5dc128f4d5a4ddb6825b88628a1bc28dae130
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-24 08:36:57 -0800

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(x):
+            value = my_list[i]
+            if type(value) == int:
+                print("{:d}".format(value), end='')
+                c += 1
+    except IndexError as e:
+        print(f"IndexError: {e}")
+    finally:
+        print()
+        return c
