5ae1be2ffde7506b3794c037072c48f48b9860da
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-27 03:21:28 -0800

    c

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    a = 1
-    b = 2
     from add_0 import add
 
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
