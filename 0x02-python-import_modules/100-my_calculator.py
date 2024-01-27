ee434445bbdbe1be8c4d68ef5349e2ffe5254194
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2024-01-27 03:09:55 -0800

    c

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,23 @@
 from calculator_1 import add, sub, mul, div
 from sys import argv
 
-if len(argv) != 4:
-    print("Usage: {} <a> <operator> <b>".format(argv[0]))
-    exit(1)
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
 
-a, operator, b = int(argv[1]), argv[2], int(argv[3])
+    a, operator, b = int(argv[1]), argv[2], int(argv[3])
 
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-print("{} {} {} = {}".format(a, operator, b, result))
+    print("{} {} {} = {}".format(a, operator, b, result))
